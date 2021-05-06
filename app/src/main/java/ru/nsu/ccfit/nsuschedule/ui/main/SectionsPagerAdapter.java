b0427e5683b4081b83591eb4a8a3e9d9b46a6493
Den4ick240 <zhigalovden@mail.ru>
2021-05-07 00:15:58 +0700

    Tab view with tab names consisting of date and day of week

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/main/SectionsPagerAdapter.java
@@ -10,7 +10,13 @@ import androidx.fragment.app.FragmentPagerAdapter;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import ru.nsu.ccfit.nsuschedule.R;
+
+import static java.util.Calendar.DATE;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -20,11 +26,38 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
     private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] DAYS_OF_WEEK_TITLES = new int[]{
+            R.string.sundayTab,
+            R.string.mondayTab,
+            R.string.tuesdayTab,
+            R.string.wednesdayTab,
+            R.string.thursdayTab,
+            R.string.fridayTab,
+            R.string.saturdayTab
+    };
     private final Context mContext;
+    private final int numberOfDays;
+    private final Calendar firstDate;
+    private final List<Calendar> calendarList;
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
-        super(fm, 0);
+    public SectionsPagerAdapter(Context context, FragmentManager fm, int behaviour,
+                                Calendar firstDate, int numberOfDays) {
+        super(fm, behaviour);
         mContext = context;
+        this.numberOfDays = numberOfDays;
+        this.firstDate = firstDate;
+        calendarList = getCalendarList();
+    }
+
+    private List<Calendar> getCalendarList() {
+        ArrayList<Calendar> calendars = new ArrayList<>();
+        for (int i = 0; i < numberOfDays; i++) {
+            Calendar c = Calendar.getInstance();
+            c.setTime(firstDate.getTime());
+            c.add(Calendar.DATE, i);
+            calendars.add(c);
+        }
+        return calendars;
     }
 
     @NotNull
@@ -32,18 +65,19 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        return PlaceholderFragment.newInstance(calendarList.get(position).get(Calendar.DAY_OF_MONTH));
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
+        Calendar calendar = calendarList.get(position);
+        return calendar.get(DATE) + "\n" + mContext.getResources().getString(DAYS_OF_WEEK_TITLES[calendar.get(Calendar.DAY_OF_WEEK) - 1]);
     }
 
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return calendarList.size();
     }
 }
