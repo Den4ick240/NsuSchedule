c74ab41136816a3ed6c2de21d593cdcd1f279ce4
OlesyaSokolova <tsapa23@mail.ru>
2021-05-11 00:07:17 +0700

    Added event listener to menu

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/MainActivity.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/MainActivity.java
@@ -1,16 +1,21 @@
 package ru.nsu.ccfit.nsuschedule.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 
 import ru.nsu.ccfit.nsuschedule.R;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
     private Toolbar toolbar;
-
+    private NavigationView nvDrawer;
 
 
     @Override
@@ -23,8 +28,33 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mDrawer = findViewById(R.id.drawer_layout);
 
+        nvDrawer = findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
     }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    selectDrawerItem(menuItem);
+                    return true;
+                });
+    }
 
+    public void selectDrawerItem(MenuItem menuItem) {
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mDrawer.openDrawer(GravityCompat.START);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
