94518c95db36f65b496dbfc0c2223ff87ead04bb
OlesyaSokolova <tsapa23@mail.ru>
2021-05-11 00:04:18 +0700

    Added menu

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/MainActivity.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/MainActivity.java
@@ -3,14 +3,28 @@ package ru.nsu.ccfit.nsuschedule.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import ru.nsu.ccfit.nsuschedule.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
+
+
 }
