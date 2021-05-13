7511026767f12eb14786376de284d5d1378ed070
a_valkova <love-cats-forever@mail.ru>
2021-05-14 00:32:07 +0700

    Added ability to create events, choosing day, start time and end time

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/create_event/CreateEventFragment.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/create_event/CreateEventFragment.java
@@ -1,5 +1,7 @@
 package ru.nsu.ccfit.nsuschedule.ui.create_event;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 import ru.nsu.ccfit.nsuschedule.ApplicationWithAppContainer;
 import ru.nsu.ccfit.nsuschedule.R;
 
@@ -24,6 +28,12 @@ public class CreateEventFragment extends Fragment implements View.OnClickListene
     private TextInputLayout summary;
     private TextInputLayout description;
     private TextInputLayout location;
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog startTimePickerDialog;
+    private TimePickerDialog endTimePickerDialog;
+    private Button dateButton;
+    private Button endTimeButton;
+    private Button startTimeButton;
 
 
     public static CreateEventFragment newInstance() {
@@ -39,26 +49,84 @@ public class CreateEventFragment extends Fragment implements View.OnClickListene
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button button = view.findViewById(R.id.create_event_button);
-        button.setOnClickListener(this);
+        ((Button) view.findViewById(R.id.create_event_button)).setOnClickListener(this);
+
+        dateButton = view.findViewById(R.id.date_picker_button);
+        dateButton.setOnClickListener(this);
+
         summary = view.findViewById(R.id.summary_text_input);
         description = view.findViewById(R.id.description_text_input);
         location = view.findViewById(R.id.location_text_input);
+        endTimeButton = view.findViewById(R.id.end_time_button);
+        startTimeButton = view.findViewById(R.id.start_time_button);
+
+        startTimeButton.setOnClickListener(this);
+        endTimeButton.setOnClickListener(this);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         viewModel = new ViewModelProvider(
-                this,
-                ((ApplicationWithAppContainer) getActivity().getApplication()).getAppContainer().createEventViewModelFactory
+                requireActivity(),
+                ((ApplicationWithAppContainer) Objects.requireNonNull(getActivity()).getApplication()).getAppContainer().createEventViewModelFactory
         ).get(CreateEventViewModel.class);
+        initDatePicker();
+        initEndTimePicker();
+        initStartTimePicker();
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.create_event_button) {
-            viewModel.addEvent(summary.getEditText().getText().toString(), description.getEditText().getText().toString(), location.getEditText().getText().toString());
+            addEvent();
+        }
+        if (v.getId() == R.id.date_picker_button) {
+            datePickerDialog.show();
+        }
+        if (v.getId() == R.id.start_time_button) {
+            startTimePickerDialog.show();
+        }
+        if (v.getId() == R.id.end_time_button) {
+            endTimePickerDialog.show();
         }
     }
+
+    private void addEvent() {
+        viewModel.addEvent(
+                summary.getEditText().getText().toString(),
+                description.getEditText().getText().toString(),
+                location.getEditText().getText().toString());
+    }
+
+    private void initDatePicker() {
+        viewModel.getSelectedDateString().observe(getViewLifecycleOwner(), dateButton::setText);
+        datePickerDialog = new DatePickerDialog(
+                getActivity(),
+                (datePicker, year, month, day) -> viewModel.onDateSet(year, month, day),
+                viewModel.getCurrentYear(),
+                viewModel.getCurrentMonth(),
+                viewModel.getCurrentDay());
+    }
+
+    private void initStartTimePicker() {
+        viewModel.getStartTime().observe(getViewLifecycleOwner(), startTimeButton::setText);
+        startTimePickerDialog = new TimePickerDialog(
+                getActivity(),
+                (view, hourOfDay, minute) -> viewModel.onStartTimeSet(hourOfDay, minute),
+                viewModel.getStartHour(),
+                viewModel.getStartMinute(),
+                true);
+    }
+
+    private void initEndTimePicker() {
+        viewModel.getEndTime().observe(getViewLifecycleOwner(), endTimeButton::setText);
+        endTimePickerDialog = new TimePickerDialog(
+                getActivity(),
+                (view, hourOfDay, minute) -> viewModel.onEndTimeSet(hourOfDay, minute),
+                viewModel.getEndHour(),
+                viewModel.getEndMinute(),
+                true);
+    }
+
 }
--- app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/create_event/CreateEventViewModel.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/ui/create_event/CreateEventViewModel.java
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import ru.nsu.ccfit.nsuschedule.domain.entities.Event;
 import ru.nsu.ccfit.nsuschedule.domain.entities.EventDate;
@@ -17,21 +19,35 @@ public class CreateEventViewModel extends ViewModel {
     private final AddEvent addEvent;
     private final MutableLiveData<Void> eventCreated = new MutableLiveData<>();
     private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    private final MutableLiveData<String> selectedDateString = new MutableLiveData<>();
+    private final Calendar selectedDay;
+    private final Calendar startTime;
+    private final Calendar endTime;
+    private final MutableLiveData<String> startTimeStringLiveData = new MutableLiveData<>();
+    private final MutableLiveData<String> endTimeStringLiveData = new MutableLiveData<>();
+    private final DateFormat timeFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
 
     public CreateEventViewModel(AddEvent addEvent) {
         this.addEvent = addEvent;
+        selectedDay = Calendar.getInstance();
+        startTime = Calendar.getInstance();
+        endTime = Calendar.getInstance();
+        updateSelectedDateString();
     }
 
     public void addEvent(String summary, String description, String location) {
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, 1);
+        calendar.set(getCurrentYear(), getCurrentMonth(), getCurrentDay(), getStartHour(), getStartMinute());
+        Date startDate = calendar.getTime();
+        calendar.set(getCurrentYear(), getCurrentMonth(), getCurrentDay(), getEndHour(), getEndMinute());
+        Date endDate = calendar.getTime();
         Runnable task = () ->
         {
             try {
                 addEvent.add(
                         new Event(
                                 new EventInfo(summary, description, location),
-                                new EventDate(calendar.getTime(), calendar.getTime(), Repeating.ONCE)));
+                                new EventDate(startDate, endDate, Repeating.ONCE)));
                 eventCreated.postValue(null);
             } catch (RepositoryException e) {
                 e.printStackTrace();
@@ -45,5 +61,86 @@ public class CreateEventViewModel extends ViewModel {
 
     public LiveData<Void> getEventCreated() {
         return eventCreated;
+    }
+
+    public String makeDateString(int day, int month, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        return makeDateString(calendar.getTime());
+    }
+
+    public String makeDateString(Date date) {
+        return DateFormat.getDateInstance().format(date);
+    }
+
+    public String makeDateString() {
+        return makeDateString(selectedDay.getTime());
+    }
+
+    public void onDateSet(int year, int month, int day) {
+        selectedDay.set(year, month, day);
+        updateSelectedDateString();
+    }
+
+    public LiveData<String> getSelectedDateString() {
+        return selectedDateString;
+    }
+
+    public int getCurrentDay() {
+        return selectedDay.get(Calendar.DATE);
+    }
+
+    public int getCurrentMonth() {
+        return selectedDay.get(Calendar.MONTH);
+    }
+
+    public int getCurrentYear() {
+        return selectedDay.get(Calendar.YEAR);
+    }
+
+    private void updateSelectedDateString() {
+        selectedDateString.setValue(makeDateString());
+    }
+
+    public LiveData<String> getStartTime() {
+        return startTimeStringLiveData;
+    }
+
+    public int getStartHour() {
+        return startTime.get(Calendar.HOUR_OF_DAY);
+    }
+
+    public int getStartMinute() {
+        return startTime.get(Calendar.MINUTE);
+    }
+
+    public LiveData<String> getEndTime() {
+        return endTimeStringLiveData;
+    }
+
+    public int getEndHour() {
+        return endTime.get(Calendar.HOUR_OF_DAY);
+    }
+
+    public int getEndMinute() {
+        return endTime.get(Calendar.MINUTE);
+    }
+
+    public void onStartTimeSet(int hourOfDay, int minute) {
+        startTime.set(0,0,0, hourOfDay, minute);
+        updateStartTimeString();
+    }
+
+    private void updateStartTimeString() {
+        startTimeStringLiveData.setValue(timeFormat.format(startTime.getTime()));
+    }
+
+    public void onEndTimeSet(int hourOfDay, int minute) {
+        endTime.set(0,0,0, hourOfDay, minute);
+        updateEndTimeString();
+    }
+
+    private void updateEndTimeString() {
+        endTimeStringLiveData.setValue(timeFormat.format(endTime.getTime()));
     }
 }
