d6ad646847617a85386ace1bb48c83398fca8f82
a_valkova <love-cats-forever@mail.ru>
2021-05-16 17:49:08 +0700

    added ability to create repeating events

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/AppContainer.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/AppContainer.java
@@ -2,21 +2,12 @@ package ru.nsu.ccfit.nsuschedule;
 
 import androidx.lifecycle.ViewModelProvider;
 
-import java.util.Collections;
-import java.util.Map;
-
-import ru.nsu.ccfit.nsuschedule.domain.entities.Repeating;
-
 public class AppContainer {
-    public final Map<Repeating, String> repeatingEnumTranslationMap;
-
     public final ViewModelProvider.Factory scheduleDayViewModelFactory;
     public final ViewModelProvider.Factory createEventViewModelFactory;
 
-    public AppContainer(Map<Repeating, String> repeatingEnumTranslationMap,
-                        ViewModelProvider.Factory scheduleDayViewModelFactory,
+    public AppContainer(ViewModelProvider.Factory scheduleDayViewModelFactory,
                         ViewModelProvider.Factory createEventViewModelFactory) {
-        this.repeatingEnumTranslationMap = Collections.unmodifiableMap(repeatingEnumTranslationMap);
         this.scheduleDayViewModelFactory = scheduleDayViewModelFactory;
         this.createEventViewModelFactory = createEventViewModelFactory;
     }
