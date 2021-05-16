88459c7091618ca9a3d07f208bfc8f7a8d857e37
a_valkova <love-cats-forever@mail.ru>
2021-05-16 15:34:51 +0700

    changed Event: separated event ending time from event repeating date

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/domain/entities/EventDate.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/domain/entities/EventDate.java
@@ -5,14 +5,21 @@ import java.util.Date;
 public class EventDate {
 
     public EventDate(Date startDate, Date endDate, Repeating repeating) {
+
+        this(startDate, endDate, repeating, endDate);
+    }
+
+    public EventDate(Date startDate, Date endDate, Repeating repeating, Date repeatUntilDate) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.repeating = repeating;
+        this.repeatUntilDate = repeatUntilDate;
     }
 
     private final Repeating repeating;
     private final Date startDate;
     private final Date endDate;
+    private final Date repeatUntilDate;
 
     public Repeating getRepeating() {
         return repeating;
@@ -24,5 +31,9 @@ public class EventDate {
 
     public Date getEndDate() {
         return endDate;
+    }
+
+    public Date getRepeatUntilDate() {
+        return repeatUntilDate;
     }
 }
