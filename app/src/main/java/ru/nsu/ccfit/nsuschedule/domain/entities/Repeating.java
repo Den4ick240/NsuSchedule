3493d6e0229cb5963d825c045992d76d04f5bf5c
a_valkova <love-cats-forever@mail.ru>
2021-05-16 15:37:01 +0700

    removed useless gson annotations from Repeating enum

--- app/src/main/java/ru/nsu/ccfit/nsuschedule/domain/entities/Repeating.java
+++ app/src/main/java/ru/nsu/ccfit/nsuschedule/domain/entities/Repeating.java
@@ -1,24 +1,10 @@
 package ru.nsu.ccfit.nsuschedule.domain.entities;
 
-import com.google.gson.annotations.SerializedName;
-
 public enum Repeating {
-
-    @SerializedName("0")
     ONCE,
-
-    @SerializedName("1")
     DAY,
-
-    @SerializedName("2")
     MONTH,
-
-    @SerializedName("3")
     WEEK,
-
-    @SerializedName("4")
     TWO_WEEK,
-
-    @SerializedName("5")
     YEAR
 }
