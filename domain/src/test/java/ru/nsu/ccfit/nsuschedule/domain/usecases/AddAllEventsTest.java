85fc38c69949f2016f9f668957651e867d1af738
Den4ick240 <zhigalovden@mail.ru>
2021-05-29 23:27:53 +0700

    renamed extracted package and configured sonarqube and jacoco

---
+++ domain/src/test/java/ru/nsu/ccfit/nsuschedule/domain/usecases/AddAllEventsTest.java
@@ -0,0 +1,9 @@
+package ru.nsu.ccfit.nsuschedule.domain.usecases;
+
+import junit.framework.TestCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddAllEventsTest extends TestCase {
+
+}
